c9e7035b91b4a036d9d2db3fa9391c472fa2fbd0
Alberto Gualis <agualis@gmail.com>
2025-06-02 14:00:29 +0200

    Debug sepolia env

--- e2e/config/e2e.constants.ts
+++ e2e/config/e2e.constants.ts
@@ -3,6 +3,7 @@
 import { baseSepolia, Chain } from "wagmi/chains";
 
 export const isE2eTestEnabled = process.env.NEXT_PUBLIC_E2E_TEST_ENABLED === "true";
+console.log("NEXT_PUBLIC_BASE_SEPOLIA CI: ", process.env.NEXT_PUBLIC_BASE_SEPOLIA);
 if (isE2eTestEnabled) console.warn("E2E tests are enabled. This should only be used in a testing environment.");
 export const baseSepoliaE2eForkUrl = "http://127.0.0.1:8545";
 
