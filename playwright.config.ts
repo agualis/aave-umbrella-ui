f857991f6d45d81ef554dff7dcc88012465cc8b3
Alberto Gualis <agualis@gmail.com>
2025-06-02 18:00:45 +0200

    Increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout: 15 * 1000, // Default timeout for each test
+  timeout: 30 * 1000, // Default timeout for each test
   globalTimeout: 60 * 1000, // Global timeout for all tests
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
